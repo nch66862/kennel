974f8ec451c084b0a34c6949519e328e60dc40c8
Nick Carver <nicholascarver2@outlook.com>
2021-02-24 15:36:51 -0600

    the animals render to the DOM

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,13 +1,31 @@
 import React from "react"
 import "./Kennel.css"
+import { AnimalCard } from "./animal/AnimalCard"
 
-export const Kennel = () => (
+export const Kennel = () => {
+const kennel = {
+    title: "Nashville Kennels",
+    locations: [
+        {
+            name: "Nashville North"
+        }
+    ]
+}
+return (
     <>
-        <h2>Nashville Kennels</h2>
+        <h2 className="spinngingTitle">{kennel.title}</h2>
         <small>Loving care when you're not there.</small>
         <address>
-            <div>Visit Us at the Nashville North Location</div>
+            <div>Visit Us at the {kennel.locations[0].name} Location</div>
             <div>500 Puppy Way</div>
         </address>
+
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
     </>
 )
+}
