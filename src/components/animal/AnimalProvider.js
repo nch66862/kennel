9a1133d0fe1b426ee815f212b8d9e13aa7c301d0
Nick Carver <nicholascarver2@outlook.com>
2021-02-25 15:11:47 -0600

    broke the code by changing the database

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -10,7 +10,7 @@ export const AnimalProvider = (props) => {
     const getAnimals = () => {
         return fetch("http://localhost:8088/animals?_expand=location")
             .then(res => res.json())
-            .then(setAnimals)
+            .then(animalArray => setAnimals(animalArray))
     }
 
     const addAnimal = animalObj => {
