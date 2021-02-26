566fad2e02763efe11a38db8b4b2707394ddd8bb
Nick Carver <nicholascarver2@outlook.com>
2021-02-26 17:24:25 -0600

    tried to render the name of the logged in user to the DOM. no result yet but no error

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,39 +2,38 @@ import { React, useContext, useEffect, useState } from "react";
 import { CustomerContext } from "./customer/CustomerProvider";
 import { PropsAndState } from './PropsAndState'
 
+let i = 0
 export const Home = () => {
-    let [loggedInUser, setUser] =  useState({
-        name: "you are not logged in"
-    })
+    const userId = parseInt(localStorage.getItem("kennel_customer"))
+    const [user, setUser] = useState({})
+
     const { customers, getCustomers } = useContext(CustomerContext)
 
     useEffect(() => {
+        console.log("useEffect")
+        // debugger
         getCustomers()
-            .then(() => {
-                const userId = parseInt(localStorage.getItem("kennel_customer"))
-                console.log(userId)
-                console.log(loggedInUser)
-                console.log(customers)
-                const newestUser = customers.find(customer => customer.id === userId)
-                if (userId) {
-                    if (newestUser !== undefined){
-                        setUser(newestUser)
-                    }
-                }
-                console.log(newestUser)
-            })
     }, [])
+
+    const newestUser = customers.find(customer => customer.id === userId)
+
+    if (newestUser !== undefined && i < 2) {
+        console.log(newestUser)
+        // debugger
+        setUser(newestUser)
+        i++
+    }
 
     return (
         <>
             <h2>Nashville Kennels</h2>
             <small>Loving care when you're not there.</small>
-            
+
             <address>
                 <div>Visit Us at the Nashville North Location</div>
                 <div>500 Puppy Way</div>
             </address>
-            <PropsAndState yourName={loggedInUser} />
+            <PropsAndState yourName={user} />
         </>
     )
 }
--- src/components/customer/CustomerProvider.js
+++ src/components/customer/CustomerProvider.js
@@ -10,7 +10,7 @@ export const CustomerProvider = (props) => {
     const getCustomers = () => {
         return fetch("http://localhost:8088/customers")
             .then(res => res.json())
-            .then(result => setCustomers(result))
+            .then(setCustomers)
     }
 
     const addCustomer = customerObj => {
