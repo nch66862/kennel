c30e8fe43116561ba059599ba1c24f722457345e
Nick Carver <nicholascarver2@outlook.com>
2021-02-26 12:07:13 -0600

    the employee form now saves new employees and renders them to the employee list upon save

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -10,6 +10,7 @@ import { EmployeeList } from "./employee/EmployeeList"
 import { LocationProvider } from "./location/LocationProvider"
 import { LocationList } from "./location/LocationList"
 import { AnimalForm } from "./animal/AnimalForm"
+import { EmployeeForm } from "./employee/EmployeeForm"
 
 export const ApplicationViews = () => {
     return (
@@ -40,9 +41,14 @@ export const ApplicationViews = () => {
                 </Route>
             </CustomerProvider>
             <EmployeeProvider>
-                <Route path="/employees">
-                    <EmployeeList />
-                </Route>
+                <LocationProvider>
+                    <Route exact path="/employees">
+                        <EmployeeList />
+                    </Route>
+                    <Route path="/employees/create">
+                        <EmployeeForm />
+                    </Route>
+                </LocationProvider>
             </EmployeeProvider>
         </>
     )
