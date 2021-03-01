89e07e6aececa380d0b92612fc6aa6b09492d9a5
Nick Carver <nicholascarver2@outlook.com>
2021-03-01 14:23:49 -0600

    fixed the message on the home screen that displayed the name of the logged in user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,23 @@
 import { React, useContext, useEffect, useState } from "react";
 import { CustomerContext } from "./customer/CustomerProvider";
-import { PropsAndState } from './PropsAndState'
 
-let i = 0
 export const Home = () => {
     const userId = parseInt(localStorage.getItem("kennel_customer"))
-    const [user, setUser] = useState({})
+    console.log('userId: ', userId);
+    const [user, setUser] = useState({name: ""})
 
     const { customers, getCustomers } = useContext(CustomerContext)
 
     useEffect(() => {
         console.log("useEffect")
-        // debugger
         getCustomers()
     }, [])
 
-    const newestUser = customers.find(customer => customer.id === userId)
-
-    if (newestUser !== undefined && i < 2) {
-        console.log(newestUser)
-        // debugger
-        setUser(newestUser)
-        i++
-    }
+    useEffect(() => {
+        console.log(customers)
+        const newestUser = customers.find(customer => customer.id === userId)
+        newestUser ? setUser(newestUser):console.log("no data")
+    }, [customers])
 
     return (
         <>
@@ -33,7 +28,8 @@ export const Home = () => {
                 <div>Visit Us at the Nashville North Location</div>
                 <div>500 Puppy Way</div>
             </address>
-            <PropsAndState yourName={user} />
+            {console.log("rendering return statement", user)}
+            <h3>Welcome, {user.name}</h3>
         </>
     )
 }
