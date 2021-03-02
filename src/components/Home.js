2c28eb455b77d8bce310e7e280e5f22946946e9d
Nick Carver <nicholascarver2@outlook.com>
2021-03-02 13:18:09 -0600

    changed back to working code

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,23 @@ import { React, useContext, useEffect, useState } from "react";
 import { CustomerContext } from "./customer/CustomerProvider";
 
 export const Home = () => {
-    // const userId = parseInt(localStorage.getItem("kennel_customer"))
-    // console.log('userId: ', userId);
-    const [user, setUser] = useState({})
+    const userId = parseInt(localStorage.getItem("kennel_customer"))
+    console.log('userId: ', userId);
+    const [user, setUser] = useState({name: ""})
 
     const { customers, getCustomers } = useContext(CustomerContext)
 
-    useEffect(() => getCustomers().then(() => {
-        const userId = parseInt(localStorage.getItem("kennel_customer"))
-        console.log("customers", customers)
-        const newestUser = customers.find(customer => customer.id === userId)
-        newestUser ? setUser(newestUser) : console.log('nobody home')
-    }), [])
+    useEffect(() => {
+        console.log("useEffect")
+        getCustomers()
+    }, [])
 
-    console.log("customers", customers)
-    
+    useEffect(() => {
+        console.log(customers)
+        const newestUser = customers.find(customer => customer.id === userId)
+        newestUser ? setUser(newestUser):console.log("no data")
+    }, [customers])
+
     return (
         <>
             <h2>Nashville Kennels</h2>
@@ -26,12 +28,8 @@ export const Home = () => {
                 <div>Visit Us at the Nashville North Location</div>
                 <div>500 Puppy Way</div>
             </address>
+            {console.log("rendering return statement", user)}
             <h3>Welcome, {user.name}</h3>
         </>
     )
 }
-    // useEffect(() => {
-    //     console.log(customers)
-    //     const newestUser = customers.find(customer => customer.id === userId)
-    //     newestUser ? setUser(newestUser):console.log("no data")
-    // }, [customers])
