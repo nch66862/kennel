a703a05fd93d57f9cfd37bbd9efd62fdce7c3f60
Nick Carver <nicholascarver2@outlook.com>
2021-03-04 10:58:24 -0600

    the employee edit button works

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -69,6 +69,9 @@ export const ApplicationViews = () => {
                     <Route path="/employees/create">
                         <EmployeeForm />
                     </Route>
+                    <Route path="/employees/edit/:employeeId(\d+)">
+                        <EmployeeForm />
+                    </Route>
                     <Route exact path="/employee/detail/:employeeId(\d+)">
                         <EmployeeDetail />
                     </Route>
